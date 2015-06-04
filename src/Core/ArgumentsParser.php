d2be2dbe3ab514636ca6396efa8f7716f8837e7f
Amine Ben hammou <webneat@gmail.com>
2015-06-04 14:28:44 +0200

    ArgumentsParser done

--- src/Core/ArgumentsParser.php
+++ src/Core/ArgumentsParser.php
@@ -1,11 +1,8 @@
 <?php namespace Qosasa\Core;
 
 use Qosasa\Core\Format;
+use Qosasa\Core\Exceptions\ArgumentsParserException;
 
-/**
- * Parse the arguments based on the format
- * and provide the data to be passed to the template.
- */
 class ArgumentsParser {
 
     /**
@@ -21,20 +18,185 @@ class ArgumentsParser {
      * @param   Format $format The format to use when parsing arguments
      * @return  void
      */
-    public function __construct(Format $format){
+    public function __construct(Format $format)
+    {
         $this->format = $format;
     }
 
     /**
      * Parses arguments using the format and returns the corresponding data.
      * 
-     * @param  string $args A string specifying the arguments
-     * @return array        The data to pass to the template
+     * @param  string $args The arguments following the syntax: tokens [--flag ...]
+     * @return array  The data to pass to the template
+     * @throws ArgumentsParserException if arguments contain an unknown flag
      */
-    public function parse($args){
-        $data = [];
-        
-        return $data;
+    public function parse($args)
+    {
+        $result = [
+            'data' => null,
+            'flags' => []
+        ];
+        // Initializing flags
+        foreach( $this->format->flags as $flag ) {
+            $result['flags'][$flag] = false;
+        }
+        $tokens = $args;
+        // Retrieve flags from the arguments
+        if( count($this->format->flags) > 0 ) {
+            $args = explode(' ', $args);
+            $i = count($args) - 1;
+            while( $i > 0 ) {
+                if( '--' == substr($args[$i], 0, 2) ) {
+                    $flag = substr($args[$i], 2);
+                    if( ! in_array($flag, $this->format->flags) ) {
+                        throw new ArgumentsParserException("Unknown flag '{$flag}'");
+                    }
+                    $result['flags'][$flag] = true;
+                    $i --;
+                } else {
+                    break;
+                }
+            }
+            $tokens = implode(' ', array_slice($args, 0, $i + 1));
+        }
+        // Parsing tokens
+        $result['data'] = $this->parseToken($tokens, $this->format);
+
+        return $result;
+    }
+
+    /**
+     * Parses the token using the given format.
+     * 
+     * @param  string $token 
+     * @param  Format $format 
+     * @return mixed  the corresponding data
+     * @throws ArgumentsParserException if the type of $format is unknown
+     */
+    public function parseToken($token, Format $format){
+        switch($format->type) {
+            case 'string':
+                return $token;
+            case 'number':
+                return $this->parseNumber($token);
+            case 'boolean':
+                return $this->parseBoolean($token, $format->name);
+            case 'array':
+                return $this->parseArray($token, $format->separator, $format->format);
+            case 'object':
+                return $this->parseObject($token, $format->separator, $format->format);
+            default:
+                throw new ArgumentsParserException("Unknown format type: '{$format->type}'");
+        }
+    }
+
+    /**
+     * Parses the token as a number.
+     * 
+     * @param  string     $token 
+     * @return int|double the numeric value of the token
+     * @throws ArgumentsParserException if the token is not numeric
+     */
+    protected function parseNumber($token)
+    {
+        if( ! is_numeric($token) ) {
+            throw new ArgumentsParserException("Unable to parse '{$token}' as number");
+        }
+        return $token + 0;
+    }
+
+    /**
+     * Parses the token as a boolean.
+     * 
+     * @param  string    $token
+     * @param  string    $name the name of the boolean field
+     * @return bool|null the boolean value of the token 
+     *                   or null if token does not have a valid boolean value.
+     */
+    protected function parseBoolean($token, $name)
+    {
+        if( in_array($token, ['yes', 'true', '1', $name]) ) {
+            return true;
+        } else if( in_array($token, ['no', 'false', '0', "!{$name}"]) ){
+            return false;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Parses the token as an array.
+     * 
+     * @param  string $token
+     * @param  string $separator
+     * @param  Format $format
+     * @return array 
+     */
+    protected function parseArray($token, $separator, Format $format)
+    {
+        $result = [];
+        $tokens = explode($separator, $token);
+        foreach($tokens as $value) {
+            array_push($result, $this->parseToken($value, $format));
+        }
+        return $result;
+    }
+
+    /**
+     * Parses the token as an object.
+     * 
+     * @param  string $token
+     * @param  string $separator 
+     * @param  array  $fields    array of Format specifying the format of every field of the object
+     * @return array
+     * @throws ArgumentsParserException if a requied field is not present
+     */
+    protected function parseObject($token, $separator, $fields)
+    {
+        $result = [];
+        $tokens = explode($separator, $token);
+        $tokensNumber = count($tokens);
+
+        $requiredFieldsIndexes = [];
+        $optionalFieldsIndexes = [];
+        foreach($fields as $index => $format) {
+            if( $format->default === null ) {
+                array_push($requiredFieldsIndexes, $index);
+            } else {
+                array_push($optionalFieldsIndexes, $index);
+            }
+        }
+        $requiredFieldsIndexesNumber = count($requiredFieldsIndexes);
+
+        if( $tokensNumber < $requiredFieldsIndexesNumber ) {
+            $requiredFields = array_map(function($index) use ($fields) {
+                    return $fields[$index]->name;
+                }, $requiredFieldsIndexes);
+            $requiredFields = implode($separator, $requiredFields);
+            throw new ArgumentsParserException("Required field missing: {$tokensNumber} given "
+                . "({$token}) but {$requiredFieldsIndexesNumber} required ({$requiredFields})");
+        }
+
+        $givenOptionalFieldsIndexes = array_slice(
+            $optionalFieldsIndexes, 0, $tokensNumber - $requiredFieldsIndexesNumber);
+        $notPresentFieldsIndexes = array_slice(
+            $optionalFieldsIndexes, $tokensNumber - $requiredFieldsIndexesNumber);
+        $givenFieldsIndexes = array_merge($requiredFieldsIndexes, $givenOptionalFieldsIndexes);
+        sort($givenFieldsIndexes);
+
+        // Fill the given fields
+        for( $i = 0; $i < $tokensNumber; $i ++) {
+            $fieldFormat = $fields[$givenFieldsIndexes[$i]];
+            $result[$fieldFormat->name] = $this->parseToken($tokens[$i], $fieldFormat);
+        }
+
+        // Fill other fields with default values
+        foreach( $notPresentFieldsIndexes as $index ) {
+            $fieldFormat = $fields[$index];
+            $result[$fieldFormat->name] = $fieldFormat->default;
+        }
+
+        return $result;
     }
 
 }
--- tests/unit/Core/ArgumentsParserTest.php
+++ tests/unit/Core/ArgumentsParserTest.php
@@ -2,86 +2,326 @@
 
 use Qosasa\Core\ArgumentsParser;
 use Qosasa\Core\Format;
-use Qosasa\Core\Exceptions\InvalidArguments;
+use Qosasa\Core\Exceptions\ArgumentsParserException;
 
 class ArgumentsParserTest extends PHPUnit_Framework_TestCase {
-
-    public function testParsingArgumentWithStringFormat(){
+    
+    protected function makeFormat($attrs)
+    {
         $format = new Format;
-        $format->type = 'string';
-        $format->flags = ['flagA', 'flagB'];
-
-        $parser = new ArgumentsParser($format);
-        
-        $data = $parser->parse('foo:bar,zoo');
-        $this->assertEquals([
-            'data' => 'foo:bar,zoo',
-            'flagA' => false,
-            'flagB' => false
-        ], $data);
-
-        $data = $parser->parse('some-text --flagB');
-        $this->assertEquals([
-            'data' => 'some-text',
-            'flagA' => false,
-            'flagB' => true
-        ], $data);
-
-        $data = $parser->parse('some-text --flagB --flagA');
-        $this->assertEquals([
-            'data' => 'some-text',
-            'flagA' => true,
-            'flagB' => true
-        ], $data);
-
+        foreach ($attrs as $attr => $value) {
+            $format->$attr = $value;
+        }
+        return $format;
     }
 
-    public function testParsingNumericArgumentWithNumberFormat(){
-        $format = new Format;
-        $format->type = 'number';
+    public function testParseNumber()
+    {
+        $parser = new ArgumentsParser(new Format);        
+        $numberFormat = $this->makeFormat(['type' => 'number']);
 
-        $parser = new ArgumentsParser($format);
-        
-        $data = $parser->parse('0123');
-        $this->assertEquals([ 'data' => 123 ], $data);
+        $data = $parser->parseToken('0123', $numberFormat);
+        $this->assertEquals(123, $data);
 
-        $data = $parser->parse('1.75');
-        $this->assertEquals([ 'data' => 1.75 ], $data);
+        $data = $parser->parseToken('1.75', $numberFormat);
+        $this->assertEquals(1.75, $data);
     }
 
     /**
-     * @expectedException InvalidArguments
+     * @expectedException Qosasa\Core\Exceptions\ArgumentsParserException
+     * @expectedExceptionMessage Unable to parse 'non-numeric' as number
      */
-    public function testParsingNonNumericArgumentWithNumberFormat(){
-        $format = new Format;
-        $format->type = 'number';
+    public function testParseWrongNumber()
+    {
+        $parser = new ArgumentsParser(new Format);        
+        $numberFormat = $this->makeFormat(['type' => 'number']);
+        $data = $parser->parseToken('non-numeric', $numberFormat);
+    }
 
-        $parser = new ArgumentsParser($format);
+    public function testParseBoolean()
+    {
+        $parser = new ArgumentsParser(new Format);
+        $booleanFormat = $this->makeFormat([
+            'name' => 'field-name',
+            'type' => 'boolean'
+        ]);
+
+        foreach ( ['yes', 'true', '1', 'field-name'] as $value) {
+            $data = $parser->parseToken($value, $booleanFormat);
+            $this->assertTrue($data);
+        }
+
+        foreach ( ['no', 'false', '0', '!field-name'] as $value) {
+            $data = $parser->parseToken($value, $booleanFormat);
+            $this->assertFalse($data);
+        }
+
+        $data = $parser->parseToken('notBoolean', $booleanFormat);
+        $this->assertNull($data);
+    }
+
+    public function testParseSimpleArray()
+    {
+        $parser = new ArgumentsParser(new Format);
+        $arrayFormat = $this->makeFormat([
+            'type'      => 'array',
+            'separator' => ',',
+            'format'    => $this->makeFormat([ 'type' => 'string' ])
+        ]);
+
+        $data = $parser->parseToken('foo,bar:one,,bar:two', $arrayFormat);
+        $this->assertEquals(['foo', 'bar:one', '', 'bar:two'], $data);
+    }
+
+    public function testParseSimpleObject()
+    {
+        $objectFormat = $this->makeFormat([
+            'type'      => 'object',
+            'separator' => ':',
+            'format'    => [
+                $this->makeFormat([
+                    'name' => 'name',
+                    'type' => 'string'
+                ]),
+                $this->makeFormat([
+                    'name' => 'age',
+                    'type' => 'number'
+                ]),
+                $this->makeFormat([
+                    'name'    => 'admin',
+                    'type'    => 'boolean',
+                    'default' => false
+                ]),
+                $this->makeFormat([
+                    'name'    => 'baaka',
+                    'type'    => 'boolean',
+                    'default' => true
+                ])
+            ]
+        ]);
+
+        $parser = new ArgumentsParser(new Format);
+
+        $data = $parser->parseToken('amine:25', $objectFormat);
+        $this->assertEquals([
+            'name' => 'amine',
+            'age' => 25,
+            'admin' => false,
+            'baaka' => true
+            ], $data);
+
+        $data = $parser->parseToken('amine:25:!admin:false', $objectFormat);
+        $this->assertEquals([
+            'name' => 'amine',
+            'age' => 25,
+            'admin' => false,
+            'baaka' => false
+            ], $data);
+
+    }
+
+    /**
+     * @expectedException Qosasa\Core\Exceptions\ArgumentsParserException
+     * @expectedExceptionMessage Required field missing: 1 given (amine) but 2 required (name:age)
+     */
+    public function testParseObjectWithMissingFields()
+    {
+        $objectFormat = $this->makeFormat([
+            'type'      => 'object',
+            'separator' => ':',
+            'format'    => [
+                $this->makeFormat([
+                    'name' => 'name',
+                    'type' => 'string'
+                ]),
+                $this->makeFormat([
+                    'name' => 'age',
+                    'type' => 'number'
+                ]),
+                $this->makeFormat([
+                    'name'    => 'admin',
+                    'type'    => 'boolean',
+                    'default' => false
+                ]),
+                $this->makeFormat([
+                    'name'    => 'baaka',
+                    'type'    => 'boolean',
+                    'default' => true
+                ])
+            ]
+        ]);
+
+        $parser = new ArgumentsParser(new Format);
+
+        $data = $parser->parseToken('amine', $objectFormat);
+    }
+
+    public function testParseUsingComplexFormat()
+    {
+        $format = $this->makeFormat([
+            'type'      => 'object',
+            'separator' => ':',
+            'flags'     => ['h', 'compact'],
+            'format'    => [
+                $this->makeFormat([
+                    'name' => 'name',
+                    'type' => 'string'
+                ]),
+                $this->makeFormat([
+                    'name'      => 'parents',
+                    'type'      => 'array',
+                    'separator' => ',',
+                    'default'   => [],
+                    'format'    => $this->makeFormat([ 'type' => 'string' ])
+                ]),
+                $this->makeFormat([
+                    'name'      => 'interfaces',
+                    'type'      => 'array',
+                    'separator' => ',',
+                    'default'   => [],
+                    'format'    => $this->makeFormat([ 'type' => 'string' ])
+                ]),
+                $this->makeFormat([
+                    'name'      => 'attrs',
+                    'type'      => 'array',
+                    'separator' => ',',
+                    'format'    => $this->makeFormat([
+                        'type'      => 'object',
+                        'separator' => '.',
+                        'format'    => [
+                            $this->makeFormat([
+                                'name' => 'name',
+                                'type' => 'string'
+                            ]),
+                            $this->makeFormat([
+                                'name' => 'type',
+                                'type' => 'string'
+                            ]),
+                            $this->makeFormat([
+                                'name'    => 'static',
+                                'type'    => 'boolean',
+                                'default' => false
+                            ]),
+                            $this->makeFormat([
+                                'name'    => 'initial',
+                                'type'    => 'string',
+                                'default' => ''
+                            ])
+                        ]
+                    ])
+                ])   
+            ]
+        ]);
         
-        $data = $parser->parse('0123');
-        $this->assertEquals([ 'data' => 123 ], $data);
+        $parser = new ArgumentsParser($format);
 
-        $data = $parser->parse('1.75');
-        $this->assertEquals([ 'data' => 1.75 ], $data);
+        $data = $parser->parse('Baaka:Person:StupidInterface,CrazyInterface:level.int,count.int.static.0,favoriteColor.string.false.blue --compact');
+        $this->assertEquals([
+            'data'  => [
+                'name'       => 'Baaka',
+                'parents'    => [ 'Person' ],
+                'interfaces' => [ 'StupidInterface', 'CrazyInterface' ],
+                'attrs'      => [
+                    [ 
+                        'name'    => 'level',
+                        'type'    => 'int',
+                        'static'  => false,
+                        'initial' => ''
+                    ], [ 
+                        'name'    => 'count',
+                        'type'    => 'int',
+                        'static'  => true,
+                        'initial' => 0
+                    ], [ 
+                        'name'    => 'favoriteColor',
+                        'type'    => 'string',
+                        'static'  => false,
+                        'initial' => 'blue'
+                    ],
+                ]
+            ],
+            'flags' => [
+                'compact' => true,
+                'h'       => false
+            ]
+            ], $data);
+        
+        $data = $parser->parse('Person:name.string,count.int.static,friends.vector< Person* >');
+        $this->assertEquals([
+            'data'  => [
+                'name'       => 'Person',
+                'parents'    => [],
+                'interfaces' => [],
+                'attrs'      => [
+                    [ 
+                        'name'    => 'name',
+                        'type'    => 'string',
+                        'static'  => false,
+                        'initial' => ''
+                    ], [ 
+                        'name'    => 'count',
+                        'type'    => 'int',
+                        'static'  => true,
+                        'initial' => ''
+                    ], [ 
+                        'name'    => 'friends',
+                        'type'    => 'vector< Person* >',
+                        'static'  => false,
+                        'initial' => ''
+                    ]
+                ]
+            ],
+            'flags' => [
+                'compact' => false,
+                'h'       => false
+            ]
+            ], $data);
+
     }
 
-    public function testParsingArgumentsWithSimpleArrayFormat(){
-        $format = new Format;
-        $format->type = 'array';
-        $format->separator = ',';
-        $format->format = new Format;
-        $format->format->type = 'string';
+    /**
+     * @expectedException Qosasa\Core\Exceptions\ArgumentsParserException
+     * @expectedExceptionMessage Unknown flag 'foo'
+     */
+    public function testParseWithUnknownFlag()
+    {
+        $format = $this->makeFormat([
+            'type'      => 'string',
+            'flags'     => ['bar', 'baz']
+        ]);
         
         $parser = new ArgumentsParser($format);
-        $data = $parser->parse('foo,bar:one,,bar:two');
-        $this->assertEquals([ 
-            'data' => ['foo', 'bar:one', '', 'bar:two']
-        ], $data);
+
+        $data = $parser->parse('my --awesome string --foo --baz');
     }
 
-    public function testParsingArgumentsWithSimpleObjectFormat(){
-        // $parser = new ArgumentsParser;
-    }
+    public function testParseStringWithFlags()
+    {
+        $format = $this->makeFormat([
+            'type'      => 'string',
+            'flags'     => ['bar', 'baz']
+        ]);
+        
+        $parser = new ArgumentsParser($format);
 
+        $data = $parser->parse('my --awesome string --baz');
+        $this->assertEquals([
+            'data'  => 'my --awesome string',
+            'flags' => [
+                'bar' => false,
+                'baz' => true
+            ]
+            ], $data);
+        
+        $data = $parser->parse('--my awesome string --bar');
+        $this->assertEquals([
+            'data'  => '--my awesome string',
+            'flags' => [
+                'bar' => true,
+                'baz' => false
+            ]
+            ], $data);
+        
+    }
 
 }
